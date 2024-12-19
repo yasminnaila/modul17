8bedc37bfb0f3f22b34a54b757b3dae22b2a8968
yasminnaila <nailayasmin149@gmail.com>
2024-12-19 20:47:12 +0700

    first commit

---
+++ MODUL14YASMIN/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home', [HomeController::class, 'index'])->name('home');
+
+Route::get('profile', ProfileController::class)->name('profile');
+
+Route::resource('employees', EmployeeController::class);
+
+Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('employees.edit');
+Route::put('/employees/{employee}', [EmployeeController::class, 'update'])->name('employees.update');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
